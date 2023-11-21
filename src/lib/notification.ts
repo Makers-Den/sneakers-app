5f46294d7dbf448cec107d22d25c542f20a61885
Mateusz Ziomek <mateusz.ziomek@makersden.io>
2023-11-21 12:13:35 +0100

    feat: add push notifications

--- src/lib/notification.ts
+++ src/lib/notification.ts
@@ -1,4 +1,11 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
 import { z } from "zod";
+import { createNamedLogger } from "./log";
+
+const logger = createNamedLogger("notification");
 
 export type NotificationData = z.infer<typeof notificationDataSchema>;
 
@@ -17,3 +24,44 @@ export const notificationDataSchema = z.union([
 ]);
 
 export const createNotificationData = (data: NotificationData) => data;
+
+export async function registerForPushNotificationsAsync() {
+  if (!Device.isDevice) {
+    logger.info("Not a physical device, Push Notifications not allowed");
+    return;
+  }
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== "granted") {
+    logger.info("Notification permissions not granted");
+    return;
+  }
+
+  const easProjectId = Constants.expoConfig?.extra?.eas.projectId;
+  if (!easProjectId) {
+    logger.info("EAS project id is missing");
+    return;
+  }
+
+  const expoPushToken = await Notifications.getExpoPushTokenAsync({
+    projectId: easProjectId,
+  });
+
+  return expoPushToken.data;
+}
